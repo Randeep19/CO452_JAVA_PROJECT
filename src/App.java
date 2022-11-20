159e4838ae1a7fc125d732fc1dd88ed0f877ff36
Randeep19 <117176818+Randeep19@users.noreply.github.com>
2022-11-20 21:47:57 +0000

    playcount over given number of plays

--- src/App.java
+++ src/App.java
@@ -4,9 +4,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 
-
-public class App
-{
+public class App{
 
     public static void main(String[] args) throws NumberFormatException, IOException
     {
