1f9ad4236e7734f86da4e7b302b13c446865e773
Randeep19 <117176818+Randeep19@users.noreply.github.com>
2022-11-20 20:40:16 +0000

    add, remove a song and print list

--- src/App.java
+++ src/App.java
@@ -1,49 +1,61 @@
 import java.util.ArrayList;
 
-import org.jcp.xml.dsig.internal.SignerOutputStream;
 
-public class App{
+
+public class App
+{
     public static void main(String[] args)
     {
         System.out.println("Songlist is Here");
 
-        Songs 1 = new Songs(Song_Title- "The Last Ride",  Artist_Name- "Sidhu Mose Wala", Play_Count- 58,879,374);
-        Songs 2 = new Songs(Song_Title- "Umbrella",  Artist_Name- "Rihanna", Play_Count- 985,676,239);
-        Songs 3 = new Songs(Song_Title- "Without You",  Artist_Name- "The Kid Laroi", Play_Count- 768,602,433);
-        Songs 4 = new Songs(Song_Title- "Impress",  Artist_Name- "Ranjit Bawa", Play_Count- 7,166,884);
-        Songs 5 = new Songs(Song_Title- "Game Over",  Artist_Name- "Karan Aujla", Play_Count- 11,035,382);
-        Songs 6 = new Songs(Song_Title- "under The Influence",  Artist_Name- "Chris Brown", Play_Count- 412,288,623);
-        Songs 7 = new Songs(Song_Title- "Start Over",  Artist_Name- "Imagine Dragons", Play_Count- 78,148,369);
-        Songs 8 = new Songs(Song_Title- "Excuses",  Artist_Name- "AP Dhillon", Play_Count- 219,379,636);
-        Songs 9 = new Songs(Song_Title- "Trust",  Artist_Name- "justin Beiber", Play_Count- 68,989,464);
-        Songs 10 = new Songs(Song_Title- "July",  Artist_Name- "Noah Cyrus", Play_Count- 642,969,965);
-        Songs 11 = new Songs(Song_Title- "Falling",  Artist_Name- "Harry Styles", Play_Count- 858,162,822);
+        Songs a1 = new Songs ("The_Last_Ride" ,  "Sidhu_Mose_Wala",  58879374);
+        a1.print();
+        Songs b2 = new Songs ( "Umbrella" ,  "Rihanna",  985676239);
+        b2.print();
+        Songs c3 = new Songs ( "Without You" , "The Kid Laroi", 768602433);
+        c3.print();
+        Songs d4 = new Songs ( "Impress" , "Ranjit Bawa",  7166884);
+        d4.print();
+        Songs e5 = new Songs ( "Game_Over" ,  "Karan_Aujla",  11035382);
+        e5.print();
+        Songs f6 = new Songs ( "under_The_Influence" , "Chris_Brown", 412288623);
+        f6.print();
+        Songs g7 = new Songs ( "Start_Over" , "Imagine_Dragons", 78148369);
+        g7.print();
+        Songs h8 = new Songs ( "Excuses" , "AP_Dhillon",219379636);
+        h8.print();
+        Songs i9 = new Songs ( "Trust" , "justin_Beiber", 68989464);
+        i9.print();
+        Songs j10 = new Songs ( "July" , "Noah_Cyrus", 642969965);
+        j10.print();
+        Songs k11 = new Songs ( "Falling" , "Harry_Styles", 858162822);
+        k11.print();
+    
 
-        ArrayList<Songs> Song = new ArrayList<Songs>(16);
+        ArrayList<Songs> music = new ArrayList<Songs>(16);
         
-        Song.add(1);
-        Song.add(2);
-        Song.add(3);
-        Song.add(4);
-        Song.add(5);
-        Song.add(6);
-        Song.add(7);
-        Song.add(8);
-        Song.add(9);
-        Song.add(10);
-        Song.add(11);
+        music.add(a1);
+        music.add(b2);
+        music.add(c3);
+        music.add(d4);
+        music.add(e5);
+        music.add(f6);
+        music.add(g7);
+        music.add(h8);
+        music.add(i9);
+        music.add(j10);
+        music.add(k11);
 
-        System.out.println(Song.get(8));
-        Song.remove(8);
+        music.remove(h8);
         System.out.println("The elimination of Song is done");
 
-        for(Songs song : Song)
+        
+        for( Songs Song: music)
         {
             Song.print();
         }
+        
 
-
-
-       
-
+    }
 }
+
--- src/Songs.java
+++ src/Songs.java
@@ -1,4 +1,31 @@
+public class Songs{
+   
+    private String Song_Title;
+    private String Artist_Name;
+    private int Plays;
 
-public class Songs {
+    public Songs (String Song_Title, String Artist_Name, int Plays)
+    {
+        this.Song_Title =Song_Title;
+        this.Artist_Name = Artist_Name;
+        this.Plays = Plays;
+    }
 
+    public void print()
+    {
+        System.out.print("Song: " + this.Song_Title + "\t");
+        System.out.print("Artist: " + this.Artist_Name  +"\t");
+        System.out.println("Plays: " + this.Plays);
+    }
+
+    public String getName()
+        {
+            return this.Song_Title;
+        
+        }
+        public int getPlays()
+        {
+            return this.Plays;
+        }
+    
 }
